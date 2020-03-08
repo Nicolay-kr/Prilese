0aea68757c725b6424b8ea494257c7dfa337fbf8
Nicolay <nicolay.krischenovich@gmail.com>
2020-03-08 19:30:34 +0300

    Make layout homepage

--- prilese/shop/views.py
+++ prilese/shop/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'shop/index.html')
